91af69d68e86c3576b63d3c42c258d91af456a31
jay <hereisjayant@gmail.com>
2020-08-28 00:21:27 +0530

    Tested FSM, successfully changing states

--- fsm.v
+++ fsm.v
@@ -294,3 +294,17 @@ module control(   //inputs to fsm
 
 
 endmodule
+
+
+//------------------------------------------------------------------------------
+//helper modules
+
+module vDFF(clk,D,Q);
+  parameter n=1;
+  input clk;
+  input [n-1:0] D;
+  output [n-1:0] Q;
+  reg [n-1:0] Q;
+  always @(posedge clk)
+    Q <= D;
+endmodule
